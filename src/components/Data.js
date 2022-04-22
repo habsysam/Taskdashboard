7ec9469f5e74a4b316bc29727ce3f937e3a7f22e
Habsy Sam <habsysam199@gmail.com>
2022-04-22 10:13:49 +0530

    asd

---
+++ src/components/Data.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import DatePicker from "react-horizontal-datepicker";
+
+export  default function Data() {
+   
+    const selectedDay = (val) =>{
+
+        console.log(val)
+    };
+ 
+  return (
+    <div>
+         <h6 id='yearh6'>2022</h6>
+         <div style={{wigth:'20px'}}>
+      <DatePicker getSelectedDay={selectedDay}
+    
+ 
+                  endDate={100}
+                  
+                  selectDate={new Date("2022-04-21")}
+                  selectedDay={new Date('thu')}
+                 
+                  labelFormat={"MMMM"}
+                  
+                  color={"#141414"}          
+/>
+</div>
+</div>
+  );
+}
